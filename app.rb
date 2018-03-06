2d9b74b7a3ccb2033846110eec0d8457006a002d
Ajibaji <persiancookbook@gmail.com>
2018-03-06 16:13:46 +0000

    params

--- app.rb
+++ app.rb
@@ -7,6 +7,13 @@ get '/secret' do
 	'<h1>Hey hey hey!</>'
 end
 
-get '/notcats' do
+get '/random-cat' do
+	@name = ["Amigo","Oscar","Viking"].sample
+	erb(:index)
+end
+
+get '/named-cat' do
+	p "this is a log message"
+	@name = params[:name]
 	erb(:index)
 end
